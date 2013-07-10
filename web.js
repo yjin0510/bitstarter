13cedf25b8684b1c0bb74f8ae88de40ba14db765
Yu Jin <yujin.umn@gmail.com>
2013-07-10 19:44:46 +0000

    update web.js for assignment 1.7

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var line_buf = fs.readFileSync('index.html');
+  var line_str = line_buf.toString();
+  response.send(line_str);
 });
 
 var port = process.env.PORT || 5000;
